f6efc8bbe7da1d256bbd50b6ac6f0b667c0a3597
no.luchs <74672861+noluchs@users.noreply.github.com>
2024-07-03 23:17:11 +0200

    Update vue.config.js

--- code/frontend/vue.config.js
+++ code/frontend/vue.config.js
@@ -1,14 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-// vue.config.js
-module.exports = {
-   allowedHosts: [
-      'gallery.luchsphoto.ch',
-      'localhost'],
+  transpileDependencies: true,
   devServer: {
+    allowedHosts: [
+      'gallery.luchsphoto.ch',
+      'localhost'
+    ],
     port: 3000
   }
-}
-
+});
