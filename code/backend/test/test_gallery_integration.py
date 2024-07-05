59da9a6d5edfffa39ab983bb4620602cb34dc19f
no.luchs <74672861+noluchs@users.noreply.github.com>
2024-07-05 20:52:17 +0200

    Update test_gallery_integration.py

--- code/backend/test/test_gallery_integration.py
+++ code/backend/test/test_gallery_integration.py
@@ -1,6 +1,6 @@
 import pytest
 from app import create_app, db
-from app.extensions import TestingConfig  # Import TestingConfig directly
+from app.extensions import TestingConfig
 from app.models import Gallery, Photo
 import io
 import os
@@ -36,35 +36,3 @@ def test_add_image_to_gallery(client):
     assert 'id' in data
     assert data['gallery_id'] == gallery_id
 
-def test_face_recognition(client, monkeypatch):
-    class MockBoto3Client:
-        def compare_faces(self, SourceImage, TargetImage):
-            return {
-                'FaceMatches': [{'Face': {'Confidence': 99.0}}]
-            }
-
-    monkeypatch.setattr('boto3.client', lambda *args, **kwargs: MockBoto3Client())
-
-    response = client.post('/api/gallery', json={'name': 'Test Gallery'})
-    gallery_id = response.get_json()['id']
-
-    image_path1 = os.path.join('backend/test/testfile', 'test_image1.jpg')
-    with open(image_path1, 'rb') as image_data1:
-        client.post(f'/api/gallery/{gallery_id}/photos', content_type='multipart/form-data', data={
-            'file': (image_data1, 'test_image1.jpg')
-        })
-
-    image_path2 = os.path.join('backend/test/testfile', 'test_image2.jpg')
-    with open(image_path2, 'rb') as image_data2:
-        client.post(f'/api/gallery/{gallery_id}/photos', content_type='multipart/form-data', data={
-            'file': (image_data2, 'test_image2.jpg')
-        })
-
-    response = client.post('/api/rekognition/compare', json={
-        'gallery_id': gallery_id,
-        'image_path': 'test_image1.jpg'
-    })
-    assert response.status_code == 200
-    data = response.get_json()
-    assert 'matches' in data
-    assert data['matches'] == [99.0]
